812c8e6b8fec6621003e797edf13725c306abdbd
Karan Takalkar <karant@iitk.ac.in>
2020-06-02 21:03:43 +0530

    Revert "fix: URL to String pipe to decode URLs (#837)"
    This reverts commit 02cdc801fcce7309569fa3d4ddd762615215ed12.

--- src/app/pipes/url-to-string.pipe.ts
+++ src/app/pipes/url-to-string.pipe.ts
@@ -11,7 +11,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class UrlToStringPipe implements PipeTransform {
 
   transform(url: string): any {
-    url = decodeURIComponent(url);
     const urlSubstrings: string[] = url.slice(1).split('/');
     const stringRepresentation =
       urlSubstrings
